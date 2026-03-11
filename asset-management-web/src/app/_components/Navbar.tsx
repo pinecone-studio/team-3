5ae3c8f0fa052edb6828ea31ecd4d35cfb05f2d7
urn_tgs <urn_tgs@G401-CC0094.local>
2026-03-11 14:15:32 +0800

    sidebar and navbar ui

---
+++ asset-management-web/src/app/_components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { SidebarTrigger } from "@/libs";
+
+export default function Navbar() {
+  return (
+    <header className="flex h-14 items-center gap-4 border-b bg-white px-4 sticky top-0 z-10">
+      <SidebarTrigger className="-ml-1 " />
+      <div className="flex items-center gap-2">
+        <span className="text-sm font-medium text-slate-600">
+          Ажилтны портал
+        </span>
+      </div>
+    </header>
+  );
+}
--- asset-management-web/src/app/page.tsx
+++ asset-management-web/src/app/page.tsx
@@ -1,24 +1,11 @@
-'use client'
-import { useTestQueryQuery } from '@/gql/graphql'
-import { Button, Input } from '../libs'
-
+"use client";
+import { useTestQueryQuery } from "@/gql/graphql";
+import { Button, Input } from "../libs";
 
 export default function Home() {
-  const { data, loading, error, refetch } = useTestQueryQuery()
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
+  // const { data, loading, error, refetch } = useTestQueryQuery()
+  // if (loading) return <p>Loading...</p>
+  // if (error) return <p>Error: {error.message}</p>
 
-
-  return (
-    <div>
-      <Button>asd</Button>
-      <Input />
-      {data?.testQuery.map((item) => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-          <p>{item.category}</p>
-        </div>
-      ))}
-    </div>
-  )
+  return <div>content</div>;
 }
