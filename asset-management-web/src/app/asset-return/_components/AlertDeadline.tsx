98365ffa4c6b0ff13262e6896c3b9b1805f897ac
Urantogos O <ourantogos65@gmail.com>
2026-03-12 04:14:50 -0700

    improve asset returnpage ui

--- asset-management-web/src/app/asset-return/_components/AlertDeadline.tsx
+++ asset-management-web/src/app/asset-return/_components/AlertDeadline.tsx
@@ -10,18 +10,18 @@ export default function AlertDeadline({
   deadline,
 }: AlertDeadlineProps) {
   return (
-    <div className="bg-white flex gap-4 border border-red-100 rounded-xl p-5 shadow-sm">
-      <AlertTriangle className="text-red-500 w-5 h-5 shrink-0 mt-0.5" />
+    <div className=" flex gap-4 border bg-[#FAF1F2] border-[#E38C90] rounded-xl p-2 shadow-sm">
+      <AlertTriangle className="  w-5 h-5 shrink-0 mt-0.5 text-[#222222]" />
 
       <div className="flex flex-col gap-1">
-        <h1 className="text-red-600 font-bold text-[15px]">
+        <h1 className=" text-[14px] font-medium text-[#222222]">
           Хугацаа ойртож байна
         </h1>
 
-        <p className="text-red-500 text-[14px]">
-          Та <span className="font-bold">{daysLeft} хоногийн</span> дотор бүх
-          хөрөнгийг буцааж өгөх шаардлагатай. Эцсийн хугацаа:
-          <span className="font-bold tracking-tight"> {deadline}</span>
+        <p className="leading-relaxed text-[14px] font-normal text-[#222222]/90">
+          Та <span>{daysLeft} хоногийн</span> дотор бүх хөрөнгийг буцааж өгөх
+          шаардлагатай. Эцсийн хугацаа:
+          <span> {deadline}</span>
         </p>
       </div>
     </div>
