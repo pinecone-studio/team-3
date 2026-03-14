ec4206bf959da360bee25d9cacd3c47d7cb4d151
ariuka-09 <ariuntuguldur3@gmail.com>
2026-03-14 18:26:35 +0800

    can sign the assignment, does not create pdf of the signature

--- asset-management-web/src/app/admin/page.tsx
+++ asset-management-web/src/app/admin/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { useTestQueryQuery } from "@/gql/graphql";
+// import { useTestQueryQuery } from "@/gql/graphql";
 import AdminHomePage from "./homepage/page";
 
 export default function AdminPage() {
   // const { data, loading, error, refetch } = useTestQueryQuery()
+  // console.log(data);
+
   // if (loading) return <p>Loading...</p>
   // if (error) return <p>Error: {error.message}</p>
 
--- asset-management-web/src/app/page.tsx
+++ asset-management-web/src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
-import { useTestQueryQuery } from "@/gql/graphql";
+// import { useTestQueryQuery } from "@/gql/graphql";
 import { Button, Input } from "../libs";
 import { HomePage } from "./_components/HomePage";
 
 export default function Home() {
-  // const { data, loading, error, refetch } = useTestQueryQuery()
+  // const { data, loading, error, refetch } = useTestQueryQuery();
   // if (loading) return <p>Loading...</p>
   // if (error) return <p>Error: {error.message}</p>
 
-  return <div><HomePage/></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
