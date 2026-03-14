910343601a436a49abd016739f0555cb84724976
Urantogos O <ourantogos65@gmail.com>
2026-03-14 00:14:31 -0700

    improve sidebar ui

--- asset-management-web/src/app/page.tsx
+++ asset-management-web/src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { HomePage } from "./_components/HomePage";
-
 export default function Home() {
   // const { data, loading, error, refetch } = useTestQueryQuery()
   // if (loading) return <p>Loading...</p>
   // if (error) return <p>Error: {error.message}</p>
 
-  return <div><HomePage/></div>;
+  return <div> </div>;
 }
