b9a3902765c62c29c8fe6a7b230a7698b3234c4b
ariuka-09 <ariuntuguldur3@gmail.com>
2026-03-19 16:45:52 +0800

    linked deployment backend

--- asset-management-web/src/app/_hooks/apollo-client.ts
+++ asset-management-web/src/app/_hooks/apollo-client.ts
@@ -1,9 +1,11 @@
-import { ApolloClient, InMemoryCache,  createHttpLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
-  link: createHttpLink({ 
-    uri: process.env.NODE_ENV === "development" ? "http://localhost:8787/graphql" : "https://asset-management-service.tuguldur-r.workers.dev/graphql", 
+  link: createHttpLink({
+    uri:
+      process.env.NODE_ENV === "development"
+        ? "https://asset-management-service.ariuntuguldur3.workers.dev/graphql"
+        : "https://asset-management-service.tuguldur-r.workers.dev/graphql",
   }),
   cache: new InMemoryCache(),
-})
-
+});
